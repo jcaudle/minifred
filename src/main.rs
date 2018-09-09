c60084fec1b996c1dbf66070afcfc691ec0486c6
Joseph Caudle <joseph@josephcaudle.com>
2018-09-08 23:13:08 -0400

    Get rid of print_something

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@
 #![no_main]
 
 extern crate bootloader_precompiled;
+#[macro_use]
+extern crate lazy_static;
+extern crate spin;
 extern crate volatile;
 
 use core::panic::PanicInfo;
@@ -18,7 +21,9 @@ pub fn panic(_info: &PanicInfo) -> ! {
 // Defining a new entry place
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    vga_buffer::print_something();
+    use core::fmt::Write;
+    vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
+    write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}", 42, 1.337).unwrap();
 
     loop {}
 }
